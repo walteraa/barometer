7d3cdd314d7fdd8358c0380a11e97e0550e6f007
Maysa Macedo <maysa.macedo95@gmail.com>
2018-04-02 17:50:40 -0300

    modified config setup

--- agents/BootTimeAgent/boottimeagent/utils/config.py
+++ agents/BootTimeAgent/boottimeagent/utils/config.py
@@ -6,21 +6,17 @@ from boottimeagent.exceptions.errors import  *
 class Config:
 
     def __init__(self):
-        self._load_resources()
-
-    def _load_resources(self):
-        self.map_data = {"os":{}, "aggregate":{}}
         try:
-            self.map_data['os']['auth_url'] = os.environ['OS_AUTH_URL']
-            self.map_data['os']['username'] = os.environ['OS_USERNAME']
-            self.map_data['os']['password'] = os.environ['OS_PASSWORD']
-            self.map_data['os']['project_id'] = os.environ['OS_PROJECT_ID']
-            self.map_data['os']['user_domain_name'] = os.environ['OS_USER_DOMAIN_NAME']
-            self.map_data['aggregate']['cluster_id'] = os.environ['CLUSTER_ID']
-            self.map_data['aggregate']['host'] = os.environ['HOST']
-            self.map_data['aggregate']['port'] = os.environ['PORT']
+            self.config_file = os.environ['BAR_CONFIG']
+            self.map_data = None
+            self.targets = None
         except(KeyError):
-            raise VariableMissingError("Variable  missing")
+            raise VariableMissingError("Variable BAR_CONFIG missing")
+
+    def load_resources(self):
+        if not os.path.isfile(self.config_file):
+            raise InvalidConfigFileError("Invalid config file")
+        self.map_data = json.load(open(self.config_file))
 
     def get_os(self):
         if self.map_data == None:
