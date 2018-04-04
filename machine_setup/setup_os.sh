13675df85a4dd5e38c793d6375f5dcdbdf969b64
Stenio Araujo <stenioaraujo@lsd.ufcg.edu.br>
2018-04-04 18:11:03 -0300

    Fix JSON format for private key

--- machine_setup/setup_os.sh
+++ machine_setup/setup_os.sh
@@ -2,48 +2,42 @@
 
 # Bootstrap BlackBox with needed Operating System
 # and initial packages
+set -x -e
 
-CLUSTER_ID='0'
-ENDPOINT_URL='https://endpoint.domain'
+ENDPOINT_URL='http://endpoint.domain/path'
 USERNAME='barograph'
-PROVIDER_IP="$(ifconfig | grep -A 1 'ens3' | \
-		   tail -1 | cut -d ':' -f 2 | \
-		   cut -d ' ' -f 1)"
+PROVIDER_IP=$(ip -o route get to 8.8.8.8 | \
+              sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 
-apt get install -y curl openssl python3.5-minimal \
-	python3-minimal python2.7-minimal \
-	python-minimal
+apt install -y curl openssl python3.5-minimal \
+    python3-minimal python2.7-minimal \
+    python-minimal
 
 # Create the '$USERNAME' user and allow sudo
 useradd --groups sudo --create-home --user-group $USERNAME \
-	--shell /bin/bash
+        --shell /bin/bash
 echo "$USERNAME       ALL= NOPASSWD: ALL" >> /etc/sudoers
-USER_HOME_DIR=`getent passwd $USERNAME | cut -d: -f 6`
 
 su - $USERNAME <<- EOSU
-	# Generate Private Key and add the Public Key to the
-	# authorized keys
-	mkdir -p ~/.ssh/
-	echo "y" | ssh-keygen -f ~/.ssh/id_rsa -b 2048 -t rsa \
-			   -q -N ""
-	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+    # Generate Private Key and add the Public Key to the
+    # authorized keys
+    mkdir -p ~/.ssh/
+    echo "y" | ssh-keygen -f ~/.ssh/id_rsa -b 2048 -t rsa \
+               -q -N ""
+    cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
-	# Generate POST data
-	PRIVATE_KEY=\`cat ~/.ssh/id_rsa\`
-	POST_DATA=\`cat <<- EOF
-	{
-	    "cluster_id": "$CLUSTER_ID",
-	    "node_info": {
-	        "ip": "$PROVIDER_IP",
-	        "user": "$USERNAME",
-	        "user_home_dir": "$USER_HOME_DIR",
-	        "ssh_key": "\$PRIVATE_KEY"
-	    }
-	}
-	EOF\`
+    # Generate POST data
+    PRIVATE_KEY=\$(base64 -w 0 ~/.ssh/id_rsa)
+    POST_DATA=\`cat <<- EOF
+{
+    "ip": "$PROVIDER_IP",
+    "user": "$USERNAME",
+    "ssh_key": "\$PRIVATE_KEY"
+}
+EOF\`
 
-	curl -X POST -H "Content-Type: application/json" \
-		-d "\$POST_DATA" $ENDPOINT_URL --insecure
+    curl -X POST -H "Content-Type: application/json" \
+         -d "\$POST_DATA" $ENDPOINT_URL --insecure
 
-	exit
+    exit
 EOSU
