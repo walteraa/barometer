7efc708b2c0668a16341b3bfae21fc665ba9e7e6
Stenio Araujo <stenioaraujo@lsd.ufcg.edu.br>
2018-04-06 16:36:37 -0300

    Update columns

--- barograph_screen/app.py
+++ barograph_screen/app.py
@@ -5,20 +5,6 @@ import datetime
 import os
 
 app = Flask(__name__)
-data = [{
-  "name": "bootstrap-table",
-  "value": "0.30123",
-  "count": "122",
-},
- {
-  "name": "multiple-select",
-  "value": "0.288123",
-  "count": "20",
-}, {
-  "name": "Testing",
-  "value": "0.3401235",
-  "count": "20",
-}]
 # other column settings -> http://bootstrap-table.wenzhixin.net.cn/documentation/#column-options
 columns = [
   {
@@ -27,18 +13,23 @@ columns = [
     "sortable": True,
   },
   {
-    "field": "avg",
-    "title": "Ping Average",
+    "field": "value",
+    "title": "Metric Value",
     "sortable": True,
   },
   {
-    "field": "count",
-    "title": "Node Count",
+    "field": "value_type",
+    "title": "Metric Value Type",
+    "sortable": True,
+  },
+  {
+    "field": "metric_name",
+    "title": "Metric Name",
     "sortable": True,
   },
   {
     "field": "timestamp",
-    "title": "Metric time",
+    "title": "Metric Timestamp",
     "sortable": True,
   }
 ]
@@ -46,19 +37,29 @@ columns = [
 #jdata=json.dumps(data)
 BACKEND_PORT = os.environ['BACKEND_PORT']
 BACKEND_HOST = os.environ['BACKEND_HOST']
+
+
 @app.route('/')
 def index():
-    data = requests.get("http://%s:%s/metric"%(BACKEND_HOST,BACKEND_PORT)).text
-    data = json.loads(data)
-    data = list(map(lambda m: {"timestamp": datetime.datetime.fromtimestamp(int(m["timestamp"])).strftime("%d/%m/%Y %H:%M:%S"),
-                                "cluster_id": m["cluster_id"], "avg":m["avg"], "count":m["count"]
-                                }, data))
+    def formalize_value(value):
+        return {
+            "timestamp": datetime.datetime.fromtimestamp(
+                int(value["timestamp"])).strftime("%d/%m/%Y %H:%M:%S"),
+            "cluster_id": value["cluster_id"],
+            "value": value["value"],
+            "value_type": value["value_type"],
+            "metric_name": value["metric_name"]
+        }
+
+    data = requests.get("http://%s:%s/metric" % (BACKEND_HOST, BACKEND_PORT))
+    data = json.loads(data.text)
+    data = list(map(formalize_value, data))
+
     return render_template("table.html",
-      data=data,
-      columns=columns,
-      title='Barograph Screen - Ping Metric')
+        data=data,
+        columns=columns,
+        title='Barograph Screen')
 
 
 if __name__ == '__main__':
-	#print jdata
-  app.run(debug=True,host='0.0.0.0', port=9000)
+    app.run(debug=True, host='0.0.0.0', port=9000)
